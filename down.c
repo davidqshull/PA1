84058a0f6ad86eea898ffab62bf3c4ac55345cd5
David Shull <davidqshull@gmail.com>
2017-03-23 15:31:18 -0400

    Add bubble-up heap, fix structure

--- down.c
+++ down.c
@@ -8,3 +8,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+int main() {
+    
+}
--- up.c
+++ up.c
@@ -8,3 +8,35 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+int main(void) {
+
+    char *line;
+    size_t size;
+    int array_size;
+
+    getline(&line, &size, stdin);
+    array_size = atoi(line);
+
+    int heap[array_size];
+
+    int count = 0;
+
+    while(getline(&line, &size, stdin) != -1) {
+        heap[count] = atoi(line);
+        int child = count;
+        int parent = child / 2;
+        while(heap[parent] > heap[child]) {
+            int temp = heap[parent];
+            heap[parent] = heap[child];
+            heap[child] = temp;
+            child = parent;
+            parent = child / 2;
+        }
+        count++;
+    }
+
+    for(int i = 0; i < array_size; i++) {
+        printf("%d\n", heap[i]);
+    }
+}
